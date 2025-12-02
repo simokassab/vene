dfe111874febe12d8cc3f4c4ac5025f9e56d1ef5
Mohammad KASSAB <mohammad.a.kassab@gmail.com>
2025-12-02 11:27:02 +0200

    removing force sll

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,10 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "example.com" }
+  config.action_mailer.default_url_options = { host: "161.97.162.62", port: 8000, protocol: 'http' }
+
+  # Set default URL options for all controllers
+  config.action_controller.default_url_options = { host: "161.97.162.62", port: 8000, protocol: 'http' }
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
   # config.action_mailer.smtp_settings = {
