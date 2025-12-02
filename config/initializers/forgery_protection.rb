89bf1038034a9db72c38fb1a0dce9c4859fc013c
Mohammad KASSAB <mohammad.a.kassab@gmail.com>
2025-12-02 11:29:26 +0200

    removing force sll

--- config/initializers/forgery_protection.rb
+++ config/initializers/forgery_protection.rb
@@ -1,6 +1,10 @@
 # Allow requests from nginx proxy on port 8000
 Rails.application.config.action_controller.forgery_protection_origin_check = false
 
-# Or alternatively, configure allowed request origins
-# Rails.application.config.action_controller.allow_forgery_protection = true
-# Rails.application.config.hosts << "161.97.162.62:8000"
+# Set default URL options globally for all environments
+Rails.application.routes.default_url_options = { host: '161.97.162.62', port: 8000, protocol: 'http' }
+
+# Ensure all URL helpers include the port
+Rails.application.config.to_prepare do
+  Rails.application.routes.default_url_options = { host: '161.97.162.62', port: 8000, protocol: 'http' }
+end
